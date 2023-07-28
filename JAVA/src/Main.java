0778136f693b411769eb7cafb3f645421cc08ee8
admin <joelpalacio630@gmail.com>
2023-07-27 18:18:33 -0600

    Hola Mundo en JAVA

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author JPalacio
- */
 public class Main {
-    
+    public static void main(String []args) {
+        System.out.println("Hola Mundo Desde Java");
+    }
 }
+
+
+
+
+
+
