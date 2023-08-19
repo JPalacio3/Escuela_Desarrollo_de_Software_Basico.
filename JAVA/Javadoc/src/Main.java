d3db4bbb6f11f7ee2e2abaa16b9934c81a520710
admin <joelpalacio630@gmail.com>
2023-08-18 18:00:32 -0600

    JvaDocs para documentar los archivos en Java

---
+++ JAVA/Javadoc/src/Main.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author JPalacio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    /**
+     * Esta es una clase de ejemplo que demuestra el uso de Javadoc.
+     * La clase realiza algunas operaciones matemáticas simples.
+     */
+    public class Calculadora {
+
+        /**
+         * Este método suma dos números enteros y devuelve el resultado.
+         * <h2>Función sumar()</h2>
+         * Descripción: Esta función se encarga de sumar dos números
+         * @param a El primer número a sumar.
+         * @param b El segundo número a sumar.
+         * @return La suma de los dos números.
+         */
+        public int sumar(int a, int b) {
+            return a + b;
+        }
+
+        /**
+         * Este método resta dos números enteros y devuelve el resultado.
+         *
+         * @param a El número del que se va a restar.
+         * @param b El número que se va a restar.
+         * @return La resta de los dos números.
+         */
+        public int restar(int a, int b) {
+            return a - b;
+        }
+    }
+}
--- JAVA/Juego/src/Main.java
+++ JAVA/Juego/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         OUTER:
         while (true) {
+            // Presentación del juego y opciones de nivel
             System.out.println("\n   JUEGO ADIVINA EL NÚMERO   \n");
             System.out.println(
                 "1 - Nivel Fácil      ( 10 Vidas ) \n"
@@ -14,23 +15,25 @@ public class Main {
                 + "4 - SALIR"
             );
 
+            // Capturar la opción seleccionada por el usuario
             Scanner leer = new Scanner(System.in);
             System.out.print("INGRESE UNA OPCIÓN : ");
             int opcion = leer.nextInt();
 
+            // Evaluar la opción seleccionada
             switch (opcion) {
                 case 1:
-                    jugar(10);
+                    jugar(10); // Iniciar el juego en el nivel fácil con 10 vidas
                     break;
                 case 2:
-                    jugar(7);
+                    jugar(7);  // Iniciar el juego en el nivel intermedio con 7 vidas
                     break;
                 case 3:
-                    jugar(5);
+                    jugar(5);  // Iniciar el juego en el nivel difícil con 5 vidas
                     break;
                 case 4:
                     System.out.println("CERRANDO EL PROGRAMA ... ");
-                    break OUTER;
+                    break OUTER; // Salir del juego y cerrar el programa
                 default:
                     System.out.println("Opción incorrecta");
             }
@@ -40,6 +43,8 @@ public class Main {
     static void jugar(int vidas) {
         int numeroRandom = (int) (Math.random() * 101);
         int numeroElegido = -1;
+
+        // Mostrar la cantidad de vidas disponibles al inicio del juego
         System.out.printf("\n HAS CONSEGUIDO %d VIDAS \n", vidas);
 
         Scanner leer = new Scanner(System.in);
@@ -48,6 +53,7 @@ public class Main {
             System.out.print("\nIngrese un número entre 1 y 100 : ");
             numeroElegido = leer.nextInt();
 
+            // Comparar el número elegido con el número aleatorio y ajustar las vidas
             if (numeroRandom < numeroElegido) {
                 System.out.println("\nEl número es más pequeño ! \n ");
                 vidas--;
@@ -56,6 +62,7 @@ public class Main {
                 vidas--;
             }
 
+            // Verificar si se han agotado las vidas
             if (vidas == 0) {
                 System.out.println("\n-------------------------------");
                 System.out.println("||||  PERDISTE EL JUEGO ||||");
@@ -65,6 +72,7 @@ public class Main {
             System.out.printf("\n TE QUEDAN %d VIDAS \n", vidas);
         }
 
+        // Mostrar mensaje de victoria si se adivina el número
         if (numeroElegido == numeroRandom) {
             System.out.println("\n -------------------------------");
             System.out.println("||||||  FELICIDADES, GANASTE EL JUEGO  |||||| ");
