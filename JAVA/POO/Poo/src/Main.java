049980b7f626f559ac62a0acd9a2b8555a567399
admin <joelpalacio630@gmail.com>
2023-08-18 23:08:29 -0600

    POO EN JAVA

--- JAVA/POO/Poo/src/Main.java
+++ JAVA/POO/Poo/src/Main.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
+import clases.Calculadora;
 import clases.Persona;
+import static java.lang.Math.*;
 
 /**
  *
@@ -23,13 +25,27 @@ public class Main {
         persona2.nombre = "Alberto";
         persona2.edad = 33;
         persona2.mostrarDatos();
+
+        System.out.println(Calculadora.PI); // Ejecutamos el método PI de la clase calculadora(paquete clases)
+        Calculadora.PI = 3.15; // Es posible aceder directamente a modifiar el valor de un objeto
+        System.out.println(Calculadora.PI);
+
+        System.out.println(Calculadora.sumar(40, 50)); // Ejecutamos el método sumar de la clase calculadora
+
+        System.out.println(PI); // Importar la clase Math de java completa para usar sus atributos y métodos
+        System.out.println(pow(4, 2));
     }
 }
 
 /**
  * Salida:
-Nombre: Joel
-Edad: 33 años
-Nombre: Alberto
-Edad: 33 años
+ * Nombre: Joel
+ * Edad: 33 años
+ * Nombre: Alberto
+ * Edad: 33 años
+ * 3.141592
+ * 3.15
+ * 90
+ * 3.141592653589793
+ * 16.0
  */
---
+++ JAVA/POO/Poo/src/clases/Calculadora.java
@@ -0,0 +1,12 @@
+package clases;
+
+// Definición de la clase Calculadora
+public class Calculadora {
+    // Declaración de una variable estática y constante llamada PI
+    public static double PI = 3.141592; // Esta constante representa el valor de PI
+
+    // Definir un método para ejecutar una suma
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
