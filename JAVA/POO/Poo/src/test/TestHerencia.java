a199f04feb0174c567254774a8819c2fe7111f36
admin <joelpalacio630@gmail.com>
2023-08-25 15:09:31 -0600

    Finalización curso

--- JAVA/POO/Poo/src/test/TestHerencia.java
+++ JAVA/POO/Poo/src/test/TestHerencia.java
@@ -4,8 +4,13 @@ import herencia.*;
 
 public class TestHerencia {
     public static void main(String[] args) {
-        Persona persona1 = new Persona("Joel", 'H', 33, "Guadalajara");
+        Empleados empleado1 = new Empleados("Joel", 19000);
+        System.out.println(empleado1);
 
-        System.out.println(persona1);
+        Empleados empleado2 = new Empleados("Alberto", 6000);
+        System.out.println(empleado2);
+
+        Empleados empleado3 = new Empleados("Palacio", 10000);
+        System.out.println(empleado3);
     }
 }
